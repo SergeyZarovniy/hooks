8e67db54476c70753ea0bf2a71c2e6a9201d437d
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-03-12 14:10:40 +0100

    Working with useEffect hook

--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
-import React, {useContext} from 'react';
+import React, {useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
-
-const MyContext = React.createContext();
 
 
 const App = () => {
+    const [value,setValue] = useState(0);
+    const [visible,setVisible] = useState(true);
+ if (visible) {
+     return (
+        <div>
+            <button onClick = { () => setValue((v) => value + 1)} >+</button>
+            <button onClick = { () => setVisible(false) }>hide</button>
+            <Notification/>
+        </div> 
+     );
+ } else {
+     return (
+         <button onClick = { () => setVisible(true)}>show</button>
+     )
+ }
+};
+
+const HookCounter = ({value}) => {
+
+   useEffect ( () => console.log ('mount'), [] );
+   useEffect ( () => console.log ('upDate'));
+   useEffect ( () => () => console.log ('unmount'), [] );
+
+
+    return(
+        <p>{value}</p>
+    )
+}
+
+
+const Notification = () => {
+
+ const [visible,setVisible] = useState(true);
+
+ useEffect (() => {
+     const timeout = setTimeout(() => setVisible(false),1500)
+     return () => clearTimeout(timeout);
+ }, [])
+  
+
     return (
-        <MyContext.Provider value = "Hello world">
-            <Child/>
-        </MyContext.Provider>
+        <div>
+            { visible && <p>Hello</p>}
+        </div>  
     );
 };
-
-const Child = () => {
-    const value = useContext(MyContext);
-    return <p> {value} </p>
-};
-
-
-
-
-
-
 
 
 ReactDOM.render(
