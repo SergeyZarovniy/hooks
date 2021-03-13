70e44f7d8e74157715607ace55a8b6ef6a8dccff
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-03-13 16:01:33 +0100

    Working with hooks

--- src/index.js
+++ src/index.js
@@ -46,6 +46,30 @@ const PlanetInfo = ( {id} ) => {
 };
 
 
+const PlanetInfo = ( {id} ) => {
+
+    const [name,setName] = useState(null);
+
+    useEffect (() => {
+
+    let cancelled = false;
+    
+     fetch (`https://swapi.dev/api/planets/${id}`)
+    .then(res => res.json())
+    .then(data => !cancelled && setName(data.name))
+
+    return () => cancelled = true;
+
+    },[id])
+
+    return (
+        <div>
+            {id} - {name}
+        </div>
+    )
+};
+
+
 
 
 
